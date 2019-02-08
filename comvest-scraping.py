101ca95bcbeefc126f7140a9b6f4b97745c3c120
seoruosa <thiago.giachetto@gmail.com>
2019-02-07 23:42:38 -0200

    functional script and created some functions to help the dev

--- comvest-scraping.py
+++ comvest-scraping.py
@@ -1,39 +1,67 @@
 import requests
 from bs4 import BeautifulSoup
 from collections import deque
+from urllib.parse import urlparse, urlunparse, ParseResult
+from general import *
+from time import sleep
+from random import random
+import pickle
 
-host = "http://www.comvest.unicamp.br" #/vestibulares-anteriores/1a-fase-2a-fase-comentadas/
+HOST = "http://www2.comvest.unicamp.br/" #/vestibulares-anteriores/1a-fase-2a-fase-comentadas/
+
 
 # lists with links to the website
 all_visited_pages=set()
-have_to_visit = deque([host])
+have_to_visit = deque([HOST])
 
 garbage_links = set()
-all_links = {host}
+all_links = {HOST}
+file_links = set()
 
-
+n = 0
 while len(have_to_visit)>0:
-	host = have_to_visit.popleft()
-	all_visited_pages.add(host)
-	print(host)
+	n += 1
+#	if n>1:
+#		break
+	nextLink = have_to_visit.popleft()
+	all_visited_pages.add(nextLink)
+	print(nextLink)
 
 	try:
-		page = requests.get(host, timeout=5)
+		sleep(5 * (0.8+random()))
+		page = requests.get(nextLink, timeout=5)
 		soup = BeautifulSoup(page.content, 'html.parser')
 	except:
-		garbage_links.add(host)
+		garbage_links.add(nextLink) # link that can´t visit
 		soup = ''
 
 	if soup != '':
 		for link in soup.find_all():
-		    link_string = link.get('href')
+			link_string = link.get('href')
+			if(link_string != None):
+			    link_string = extractLocationPath(link_string, HOST)
 
-		    all_links.add(link_string)
-		    if ((link_string not in all_visited_pages) and (link_string != None)):
-		    	have_to_visit.append(link_string)
+			    all_links.add(link_string)
+			    if hasFileAtLink(link_string):
+			    	file_links.add(link_string)
+			    	print("file -----{}".format(link_string))
+			    elif((link_string not in all_visited_pages) & isSubPageOfLocation(link_string, HOST)):
+			    	have_to_visit.append(link_string)
+			    	print("page -----{}".format(link_string))
 
 	print("have_to_visit: {:d}".format(len(have_to_visit)))
+
+filename = 'results.pickle'
+with open(filename, 'wb') as f:
+	pickle.dump({'garbage_links': garbage_links, 
+				'all_links': all_links, 
+				'file_links':file_links,
+				'iterations':n}, f)
+
+# http://www.jessicayung.com/how-to-use-pickle-to-save-and-load-variables-in-python/
 
 # how to discovery if a link connect to the same website
 # discovery if a link appoint to a file
 # deal with links like this /unicamp/noticias/2018/05/19/comeca-upa-2018
+
+
---
+++ general.py
@@ -0,0 +1,36 @@
+# Functions for general use
+from os.path import basename, splitext
+from urllib.parse import urlparse, urlunparse, ParseResult
+
+# Verify if a path is a file path
+def isFilePath(fullpath):
+	base = basename(fullpath)
+	name, ext = splitext(base)
+	return ext != ''
+
+assert(isFilePath('/path/some/thing') == False)
+assert(isFilePath('/path/some/thing/new.thing'))
+
+# Verify if a link addresses a file
+def hasFileAtLink(link):
+	return isFilePath(urlparse(link).path)
+
+assert(isFilePath('www.lba.bla.com/path/some/thing/new.thing?dfasdfk'))
+assert(isFilePath('www.adlf.adk.us/path/some/thing') == False)
+
+# initial location is where you start the scraping
+def extractLocationPath(link, initialLocation=''):
+	location = urlparse(initialLocation)
+	result = urlparse(link)
+	if (result.netloc == ''):
+		return urlunparse(ParseResult(location.scheme, location.netloc,result.path,'','',''))
+	else:
+		return urlunparse(ParseResult(result.scheme, result.netloc,result.path,'','',''))
+
+# Is link of the same location of the page of the scraping
+
+def isSubPageOfLocation(link, initialLocation):
+	location = urlparse(initialLocation)
+	result = urlparse(link)
+	return ((result.netloc == '') | (location.netloc == result.netloc))
+
